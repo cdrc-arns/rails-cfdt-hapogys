c4838d1db31b7ee24ceb18a33fda3ee15cf288f2
cedric arnous <cedric.arnous@gmail.com>
2022-11-21 17:11:00 +0100

    create ok

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
+  def show
+  end
+
+  def index
+    @articles = Article.all.order('created_at DESC')
+  end
+
+  def edit
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+
+    if @article.save
+      redirect_to root_path, notice: 'Votre article a été ajouté'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to root_path, notice: 'Votre article a été modifié'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:titre, :description)
+  end
+end
